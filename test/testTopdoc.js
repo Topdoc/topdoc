5cb2f77cb5bcc5d360b2fd486dfc988b85191ff2
Garth Braithwaite <garthdb@gmail.com>
2013-06-26 16:37:56 -0700

    added LICENSE file and headers

--- test/testTopdoc.js
+++ test/testTopdoc.js
@@ -1,3 +1,20 @@
+/**
+ *
+ * Copyright (c) 2013 Adobe Systems Incorporated. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 (function() {
   var Topdoc, deleteFolderRecursive, fs, path, read;
 
@@ -40,7 +57,7 @@
     it('exists', function() {
       var topdoc;
       Topdoc.should.be.ok;
-      topdoc = new Topdoc;
+      topdoc = new Topdoc(this.srcDir);
       return topdoc.should.be.instanceOf(Topdoc);
     });
     it('should accept a source in the constructor', function() {
