34e93ead57c325dd02a64d142130cbc88cf75bc2
Garth Braithwaite <garthdb@gmail.com>
2013-08-06 02:32:39 -0700

    started unit tests for cli

---
+++ test/testTopdoc-cli.js
@@ -0,0 +1,35 @@
+/**
+ *
+ * Copyright (c) 2013 Adobe Systems Incorporated. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+(function() {
+  "use strict";
+  var sys, exec, child, path;
+  sys = require('sys');
+  exec = require('child_process').exec;
+  path = require('path');
+  describe('Topdoc CLI', function() {
+    it("should validate a Topdoc css file", function(done){
+      exec("topdoc validate -s " + path.join('test', 'cases'), function (error, stdout, stderr){
+        sys.puts(stdout);
+        sys.puts(error);
+        done();
+      });
+    });
+  });
+
+}).call(this);
