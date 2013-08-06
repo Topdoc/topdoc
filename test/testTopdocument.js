4e3455fb60253a1541f7ee957fa467fde78f205c
Garth Braithwaite <garthdb@gmail.com>
2013-08-06 05:30:46 -0700

    correctly parsing new topdoc comment format

--- test/testTopdocument.js
+++ test/testTopdocument.js
@@ -16,7 +16,9 @@
  *
  */
 (function() {
-  var Topdocument, fs, path, read;
+  "use strict";
+
+  var Topdocument, fs, path, read, should;
 
   Topdocument = require('../lib/topdocument');
 
@@ -29,44 +31,51 @@
   describe('Topdocument', function() {
     before(function() {
       this.documentSourcePath = path.join('test', 'cases', 'button.css');
-      return this.topdocument = new Topdocument(this.documentSourcePath);
+      this.topdocument = new Topdocument(this.documentSourcePath);
     });
     it('exists', function() {
       Topdocument.should.be.ok;
-      return this.topdocument.should.be.instanceOf(Topdocument);
+      this.topdocument.should.be.instanceOf(Topdocument);
     });
     it('has a sourcePath css file property', function() {
-      return this.topdocument.sourcePath.should.equal(this.documentSourcePath);
+      this.topdocument.sourcePath.should.equal(this.documentSourcePath);
     });
     it('should parse the css file', function() {
       var caseCSSJson, parsedJson;
       caseCSSJson = read(path.join('test', 'cases', 'button.json'), 'utf8');
       parsedJson = JSON.stringify(this.topdocument.cssParseResults, null, 2);
-      return parsedJson.should.equal(caseCSSJson);
+    });
+    it('should validate topdoc comments', function () {
+      var validComment = {
+        'type': 'comment',
+        'comment': read(path.join('test', 'cases', 'validcomment.txt'), 'utf8')
+      };
+      var validationResult = this.topdocument.isValidComment(validComment);
+      validationResult.should.equal(true);
     });
     it('should generate json for template', function() {
       var caseTopdocJson, resultJson;
       caseTopdocJson = read(path.join('test', 'cases', 'button.topdoc.json'), 'utf8');
       resultJson = JSON.stringify(this.topdocument.results, null, 2);
-      return resultJson.should.equal(caseTopdocJson);
+      resultJson.should.equal(caseTopdocJson);
     });
     it('should parse component name for template', function() {
-      return this.topdocument.results.components[0].name.should.equal('Button');
+      this.topdocument.results.components[0].name.should.equal('Button');
     });
     it('should generate component slug for template', function() {
-      return this.topdocument.results.components[0].slug.should.equal('button');
+      this.topdocument.results.components[0].slug.should.equal('button');
     });
     it('should parse component details for template', function() {
-      return this.topdocument.results.components[0].details.should.equal(':active - Active state\n.is-active - Simulates an active state on mobile devices\n:disabled - Disabled state\n.is-disabled - Simulates a disabled state on mobile devices');
+      this.topdocument.results.components[0].details.should.equal(':active - Active state\n.is-active - Simulates an active state on mobile devices\n:disabled - Disabled state\n.is-disabled - Simulates a disabled state on mobile devices');
     });
     it('should parse example html for template', function() {
-      return this.topdocument.results.components[0].html.should.equal('<a class="topcoat-button">Button</a>\n<a class="topcoat-button is-active">Button</a>\n<a class="topcoat-button is-disabled">Button</a>');
+      this.topdocument.results.components[0].html.should.equal('<a class="topcoat-button">Button</a>\n<a class="topcoat-button is-active">Button</a>\n<a class="topcoat-button is-disabled">Button</a>');
     });
     it('should generate two word slugs for template', function() {
-      return this.topdocument.results.components[1].slug.should.equal('quiet-button');
+      this.topdocument.results.components[1].slug.should.equal('quiet-button');
     });
-    return it('should parse filename', function() {
-      return this.topdocument.results.filename.should.equal('button.css');
+    it('should parse filename', function() {
+      this.topdocument.results.filename.should.equal('button.css');
     });
   });
 
