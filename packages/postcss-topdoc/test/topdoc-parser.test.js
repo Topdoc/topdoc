5710a3786711c404c17cc1149bff19881dc19419
garthdb <garthdb@gmail.com>
2016-10-12 23:28:00 -0600

    test(Coverage): improved test coverage

--- packages/postcss-topdoc/test/topdoc-parser.test.js
+++ packages/postcss-topdoc/test/topdoc-parser.test.js
@@ -57,16 +57,37 @@ test('Should work even if no sourcePath is set', (t) => {
 
 test('Should be able to TopdocParser as a non plugin.', (t) => {
   const input = read('./fixtures/button.css');
-  const output = JSON.parse(read('./expected/button.json'));
   return postcss()
     .process(input, { from: 'fixtures/button.css' })
     .then((result) => {
-      const opts = {
-        fileData: {
-          sourcePath: 'fixtures/button.css',
-          template: 'lib/template.jade',
-        } };
-      const topdocParser = new TopdocParser(result.root, result, opts);
-      t.deepEqual(clean(topdocParser.topdoc), output);
+      const topdocParser = new TopdocParser(result.root, result);
+      t.is(topdocParser.topdoc.components.length, 2);
     });
+});
+
+test('Should be able to parse multiple css files in parallel.', (t) => {
+  const button = read('./fixtures/button.css');
+  const select = read('./fixtures/select.css');
+  return Promise.all([
+    postcss([topdoc()]).process(button, { from: 'fixtures/button.css' }),
+    postcss([topdoc()]).process(select, { from: 'fixtures/select.css' }),
+  ]).then((results) => {
+    const result = [
+      results[0].topdoc.filename,
+      results[1].topdoc.filename,
+    ];
+    const expected = [
+      'button.css',
+      'select.css',
+    ];
+    t.deepEqual(result, expected);
+  });
+});
+
+test('Should include nodes with the correct options.', (t) => {
+  const input = read('./fixtures/button.css');
+  return postcss([topdoc({ includeNodes: true })]).process(input, { from: 'fixtures/button.css' })
+  .then((result) => {
+    t.is(result.topdoc.components[0].nodes.length, 3);
+  });
 });
--- packages/postcss-topdoc/test/topdocument.test.js
+++ packages/postcss-topdoc/test/topdocument.test.js
@@ -9,3 +9,8 @@ test('Should throw an error if `name` is missing', (t) => {
     /A topdocument has to at least have a title or filename\./
   );
 });
+
+test('Should throw an error if `name` is missing', (t) => {
+  const topdoc = new TopDocument({ title: 'foo' });
+  t.is(topdoc.filename, undefined);
+});
