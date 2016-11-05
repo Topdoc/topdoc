cf8a3417de7b1b4ce8976f27f9fcaf2b5498c5dd
garthdb <garthdb@gmail.com>
2016-11-05 00:51:04 -0600

    :bug: fix(IgnoreAssets): added node_modules to the default list ignoreAssets
    ISSUES CLOSED: #45

--- src/bin/index.js
+++ src/bin/index.js
@@ -49,7 +49,7 @@ program
   .option('-c, --clobber', 'Deletes destination directory before running.', true)
   .option('-i, --ignore-assets [<file> | <list of files>]',
     `A file or comma delimeted list of files in the asset directory that should be
-    ignored when copying them over.`, _list, [/^\./, /\.pug/, /\.jade/, '/**/*.json'])
+    ignored when copying them over.`, _list, [/^\./, /^node_modules/, /\.pug/, /\.jade/, '/**/*.json'])
   .option('-a, --asset-directory [path]',
     `Path to directory of assets to copy to destination. Defaults to template directory.
     Set to false to not copy any assets.`, _valOrFalse)
