566e46e55a06ccf757262c7fe133f20be8c3fe68
Garth Braithwaite <garthdb@gmail.com>
2020-10-02 20:52:26 -0600

    fix: updated deps (#60)

--- packages/postcss-topdoc/src/index.js
+++ packages/postcss-topdoc/src/index.js
@@ -1,11 +1,9 @@
-const postcss = require('postcss');
 const TopdocParser = require('./topdoc-parser');
 /**
  * Public: PostCSS plugin allows you to inherit all the rules associated with a given selector.
  *
  * Returns a [PostCSS Plugin](http://api.postcss.org/postcss.html#.plugin) {Function}
  */
-module.exports = postcss.plugin(
-  'postcss-topdoc',
-  (opts = {}) => (css, results) => new TopdocParser(css, results, opts)
-);
+module.exports = (opts = {}) => (css, results) =>
+  new TopdocParser(css, results, opts);
+module.exports.postcss = true;
